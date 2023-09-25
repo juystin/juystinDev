cecbd1d8d79065c13cb4ae090155d8808481ef00
juystin <nguyen.2274@osu.edu>
2023-09-25 00:31:16 -0400

    Removed navbar; added hamburger menu

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+import menuIcon from "../assets/header/menu.svg"
 
 export default function Home() {
   return (
@@ -8,30 +10,29 @@ export default function Home() {
         <title>juystin.dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div className={styles.header}>
+      <div className={styles.headerContainer}>  
         <div className={styles.headerTitleContainer}>
-          <h1 className={styles.headerTitle}>juystin</h1>
+          <div className={styles.titleContainer}>
+            <div className={styles.titlePrimary}><strong>Justin Nguyen</strong></div>
+            <div className={styles.titleSecondary}>Full Stack Developer</div>
+          </div>
         </div>
-        <div className={styles.navContainer}>
-          <ul className={styles.nav}>
-            <li><a href='/contact/'>Contact</a></li>
-            <li><a href='/projects/'>Projects</a></li>
-          </ul>
-        </div>
+        <div className={styles.menuContainer}>
+            <Image
+              priority
+              src={menuIcon}
+              alt="Menu"
+            />
+          </div>
       </div>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
-          juystin.dev
+          Justin Nguyen
         </h1>
-
         <p className={styles.description}>
           Hello World!
         </p>
-
       </main>
-
     </div>
   );
 }
