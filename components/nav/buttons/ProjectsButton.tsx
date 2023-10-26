a0fd8b7c0fff1abf7e4369454f189fb5fc76609c
juystin <nguyen.2274@osu.edu>
2023-10-26 02:05:25 -0400

    Added hover effect to Projects button

--- components/nav/buttons/ProjectsButton.tsx
+++ components/nav/buttons/ProjectsButton.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { animated } from "react-spring";
 import styled from "styled-components";
 
@@ -11,21 +12,44 @@ const ButtonContainer = styled(animated.div)`
     margin: 1vh 1vw 0vh 1.4vw;
 `
 
-const ButtonTitle = styled(animated.h2)`
-    font-size: 2.2em;
+const ButtonTitle = styled(animated.h2)<{ hover: boolean }>`
+    color: ${props => props.hover ? "#E2271E" : "#0A090A"};
+    font-size: 200%;
     margin: 0 0;
+    cursor: pointer;
 `
 
-const ButtonDescription = styled(animated.p)`
-    font-size: 1.2em;
+const ButtonDescription = styled(animated.p)<{ hover: boolean }>`
+    color: ${props => props.hover ? "#E2271E" : "#0A090A"};
+    font-size: 120%;
     margin: 0 0;
+    cursor: pointer;
 `
 
 const ProjectsButton = ({}) => {
+
+    const [hover, setHover] = useState(false);
+
     return ( 
         <ButtonContainer>
-            <ButtonTitle>PROJECTS</ButtonTitle>
-            <ButtonDescription>My work</ButtonDescription>
+            <ButtonTitle 
+                hover={hover}
+                onMouseOver={() => {
+                    setHover(true);
+                }}
+                onMouseOut={() => {
+                    setHover(false);
+                }}
+            >PROJECTS</ButtonTitle>
+            <ButtonDescription 
+                hover={hover}
+                onMouseOver={() => {
+                    setHover(true);
+                }}
+                onMouseOut={() => {
+                    setHover(false);
+                }}
+            >My work</ButtonDescription>
         </ButtonContainer>
      );
 }
