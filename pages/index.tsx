b3a72162bff65850700bc86a8e6042a553166639
juystin <nguyen.2274@osu.edu>
2024-01-25 22:02:29 -0500

    Fixed iOS website tinting

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,9 +69,10 @@ export default function App() {
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 				<link rel="manifest" href="/site.webmanifest" />
-				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#EAEAEA" />
 				<meta name="msapplication-TileColor" content="#ffffff" />
-				<meta name="theme-color" content="#ff0000" />
+				<meta name="theme-color" content="#000505" />
+				<meta name="apple-mobile-web-app-status-bar-style" content="#000505"></meta>
 			</Head>
 			<ScrollToTop />
 			<GlobalTheme>
